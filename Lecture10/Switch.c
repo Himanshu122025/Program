896d638e2bc7553c54aeb4d5b79c36a0bd3f8ec0
Himanshu Prajapati <hp5122006@gmail.com>
2026-04-05 05:33:37 +0530

    calculator program

--- Lecture10/Switch.c
+++ Lecture10/Switch.c
@@ -1,7 +1,31 @@
 #include<stdio.h>
 int main ()
 {
-    int a ;
+    int a , b;
+    char choice;
     printf("Program of Calculator.");
-     
+    printf("Enter + for addition\nEnter - for substraction\nEnter * for multiplication\nEnter / for division\n");
+    printf("choice=");
+    scanf("%c",&choice);
+    printf("\nEnter two numbers\n");
+    printf("a=");
+    scanf("%d",&a);
+    printf("\nb=");
+    scanf("%d",&b);
+    switch(choice)
+    {
+        case'+':printf("The sum is %d",(a+b));
+                break;
+        case'-':printf("The substraction is %d",(a-b));
+                break;
+        case'*':printf("The multiplication is %d",(a*b));
+                break;
+        case'/':printf("The division is %d",(a/b));
+                break;
+        case'%':printf("The remender is %d",(a%b));
+                break;
+        default:printf("Ivalid choice");                                             
+    }
+    return 0;
+
 }
